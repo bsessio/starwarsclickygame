9c2aab7cd7912ccd20677772bac6c404c2af1615
Bowie Sessions <eviltoon@yahoo.com>
2019-03-25 20:22:37 -0500

    Updates

---
+++ precache-manifest.b9eab89e173e2e564641731209768b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed3a50d630777cbf5dd7",
+    "url": "/starwarsclickygame/static/js/runtime~main.69194d1c.js"
+  },
+  {
+    "revision": "3468b21e612b9afd48c7",
+    "url": "/starwarsclickygame/static/js/main.a1554132.chunk.js"
+  },
+  {
+    "revision": "1ea9d31f4e20159a1ad3",
+    "url": "/starwarsclickygame/static/js/2.8771597b.chunk.js"
+  },
+  {
+    "revision": "3468b21e612b9afd48c7",
+    "url": "/starwarsclickygame/static/css/main.891643f0.chunk.css"
+  },
+  {
+    "revision": "a911734a83657237c8c58142c2cf4ad6",
+    "url": "/starwarsclickygame/index.html"
+  }
+];
